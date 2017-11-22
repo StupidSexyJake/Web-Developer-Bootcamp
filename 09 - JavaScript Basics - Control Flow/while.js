bc219e8cfc7af9dee754d508627fe0cbcc5dc2da
Jake <jake@goldcoastmaids.com.au>
2017-11-22 16:07:10 +1000

    feat: Complete While activity
    Changes to be committed:
    	new file:   09 - JavaScript Basics - Control Flow/while.html
    	new file:   09 - JavaScript Basics - Control Flow/while.js

---
+++ 09 - JavaScript Basics - Control Flow/while.js
@@ -0,0 +1,33 @@
+console.log("Activity 1");
+var i = -10;
+while (i <= 19) {
+	console.log(i);
+	i++;
+}
+
+console.log("Activity 2");
+var i = 10;
+while (i <= 40) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+	i++;
+}
+
+console.log("Activity 3");
+var i = 300;
+while (i <= 333) {
+	if (!(i % 2 === 0)) {
+		console.log(i);
+	}
+	i++;
+}
+
+console.log("Activity 4");
+var i = 5;
+while (i <= 50) {
+	if ((i % 5 === 0 && i % 3 === 0)) {
+		console.log(i);
+	}
+	i++;
+}
