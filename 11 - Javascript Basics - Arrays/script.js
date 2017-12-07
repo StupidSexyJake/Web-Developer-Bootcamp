d4364e8ec6ba4376d7bac147609f63afb326006f
Jake <jake@goldcoastmaids.com.au>
2017-12-07 11:32:36 +1000

    feat: Add delete feature to To Do list

--- 11 - Javascript Basics - Arrays/script.js
+++ 11 - Javascript Basics - Arrays/script.js
@@ -1,14 +1,28 @@
-var todos = ["New test"];
-
+var todos = ["Example to do item"];
 var input = prompt("What would you like to do?");
 
+function listTodos() {
+    console.log("**********");
+    todos.forEach(function (todo, index) {
+        console.log(index + ": " + todo);
+    })
+    console.log("**********");
+}
+function addTodos() {
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log("New task added: " + newTodo);
+}
+function deleteTodos() {
+    var deleteTodo = prompt("Enter the index of the item to delete");
+    console.log("Task deleted: " + todos[deleteTodo]);
+    todos.splice(deleteTodo, 1);
+}
+
 while (input !== "quit") {
-    if (input === "list") {
-        console.log(todos);
-    } else if (input === "new") {
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
-    }
+    if (input === "list") { listTodos(); }
+    else if (input === "new") { addTodos(); }
+    else if (input === "delete") { deleteTodos(); }
     input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
